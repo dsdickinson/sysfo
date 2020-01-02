14a040bc487ff43683b988a38aa9e56887da188b
Steve Dickinson <steve@Colossus.local>
2020-01-02 00:44:46 -0500

    Added files to new repo.

---
+++ sysinfo.wdgtproj/project.wdgt/en.lproj/localizedStrings.js
@@ -0,0 +1,24 @@
+var localizedStrings = new Object;
+
+localizedStrings["Hello, World!"] = 'HW ';
+localizedStrings["Done"] = "Done";
+localizedStrings['label1'] = 'HW Model:';
+localizedStrings['label2'] = 'OS Type:';
+localizedStrings['label3'] = 'OS Release:';
+localizedStrings['label4'] = 'OS Revision:';
+localizedStrings['label5'] = 'Kernel Version:';
+localizedStrings['label6'] = 'OS Version:';
+localizedStrings['label7'] = 'CPU Count:';
+localizedStrings['label8'] = 'CPU Frequency:';
+localizedStrings['label9'] = 'Bus Frequency:';
+localizedStrings['label10'] = 'Physical Memory:';
+localizedStrings['label11'] = 'Virtual Memory:';
+localizedStrings['label12'] = 'Memory Size:';
+localizedStrings['labe13'] = 'HW ';
+localizedStrings['label13'] = 'Architecture:';
+localizedStrings['label14'] = 'Architecture:';
+localizedStrings['label15'] = 'CPU Vendor:';
+localizedStrings['labe16'] = 'Hostname:';
+localizedStrings['label17'] = 'CPU Brand:';
+localizedStrings['os_specific'] = 'OS Specific';
+localizedStrings['hw_specific'] = 'Hardware Specific';
